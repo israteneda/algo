53612a3521509693992f72031dbb7f1cb614e63d
Israel Teneda <israteneda@gmail.com>
2022-06-27 23:26:17 -0500

    run go fmt

--- sorting/insertion_sort.go
+++ sorting/insertion_sort.go
@@ -1,14 +1,14 @@
 package sorting
 
 func InsertionSort(elements []int) []int {
-  for index := 0; index < len(elements); index++ {
-    current := index
-    for current > 0 && elements[current] < elements[current - 1] {
-      currentValue := elements[current]
-      elements[current] = elements[current - 1]
-      elements[current - 1] = currentValue
-      current -= 1
-    }
-  }
-  return elements
+	for index := 0; index < len(elements); index++ {
+		current := index
+		for current > 0 && elements[current] < elements[current-1] {
+			currentValue := elements[current]
+			elements[current] = elements[current-1]
+			elements[current-1] = currentValue
+			current -= 1
+		}
+	}
+	return elements
 }
--- sorting/insertion_sort_test.go
+++ sorting/insertion_sort_test.go
@@ -1,15 +1,15 @@
 package sorting
 
 import (
-  "testing"
-  "github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/assert"
+	"testing"
 )
 
 func TestInsertionSort(t *testing.T) {
-  unsortedElements := []int {4, 5, 3, 2, 7}
-  expectedElements := []int {2, 3, 4, 5, 7}
+	unsortedElements := []int{4, 5, 3, 2, 7}
+	expectedElements := []int{2, 3, 4, 5, 7}
 
-  sortedElements := InsertionSort(unsortedElements)
-  
-  assert.Equal(t, sortedElements, expectedElements, "they should be equal")
+	sortedElements := InsertionSort(unsortedElements)
+
+	assert.Equal(t, sortedElements, expectedElements, "they should be equal")
 }
